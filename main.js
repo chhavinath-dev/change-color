88ccb9bc50bf85275ba6af8673141a2592d32980
chhavinath-bit <chhavinathkumar647@gmail.com>
2024-09-11 00:46:42 +0530

    change color fun game

---
+++ main.js
@@ -0,0 +1,26 @@
+let bgel = document.getElementById("bg");
+let crd = document.getElementById("colorPickerContainer");
+let bgc= document.getElementById("selectedColorHexCode");
+function firsB() {
+    bgel.style.backgroundColor = "#e0e0e0";
+    crd.style.backgroundColor = "#e0e0e0";
+    bgc.textContent= "#e0e0e0";
+}
+
+function secondB() {
+    bgel.style.backgroundColor = "#6fcf97";
+    crd.style.backgroundColor = "#6fcf97";
+     bgc.textContent= "#6fcf97";
+}
+
+function thirdB() {
+    bgel.style.backgroundColor = "#56ccf2";
+    crd.style.backgroundColor = "#56ccf2";
+     bgc.textContent= "#56ccf2";
+}
+
+function fourthB() {
+    bgel.style.backgroundColor = "#bb6bd9";
+    crd.style.backgroundColor = "#bb6bd9";
+     bgc.textContent= "#bb6bd9";
+}
